71605a528abc51760766b87e0f41fc599edf6de1
uhooi <uhooi@server.fake>
2019-07-15 21:46:57 +0900

    Xcodeプロジェクトの作成

---
+++ aruaru-lt-mobile-5-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aruaru-lt-mobile-5-demo
+//
+//  Created by uhooi on 2019/07/15.
+//  Copyright © 2019 THE Uhooi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
